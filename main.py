022402436759fa2a055f420cf0ddb88175feebc6
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2022-06-09 09:21:13 +0100

    creating the game with a while loop

--- main.py
+++ main.py
@@ -10,19 +10,46 @@ print("Welcome to the game of Rock, Paper, Scissors \n--------------------------
 
 # setting a while loop to check if the user input is in the list if not end the game
 while True:
-    player_input = input("\nplease enter your choice from the list above: ")
+    player_input = input("\nPlease enter your choice from the list above: ")
 
     # converting the inputs to uppercase
     player_choice = player_input.upper()
 
+    # Setting the user input to the words Rock, Paper, Scissors.
+    def settingplayerValue(player_value):
+        if (player_choice == game_list[0]):
+            player_value = "Rock"
+        elif (player_choice == game_list[1]):
+            player_value = "Paper"
+        elif (player_choice == game_list[2]):
+            player_value = "Scissors"
+        return player_value
+
+    
+
     # Getting the computer choice
     computer_choice = random.choice(game_list)
 
-    print(player_choice , computer_choice)
+    # Setting the computer random choice to the words Rock paper, scissors
+    def settingcomputerValue(computer_value):
+        if (computer_choice == game_list[0]):
+            computer_value = "Rock"
+        elif (computer_choice == game_list[1]):
+            computer_value = "Paper"
+        elif (computer_choice == game_list[2]):
+            computer_value = "Scissors"
+        return computer_value 
+    
+    player = settingplayerValue(player_choice)
+    computer = settingcomputerValue(computer_choice)
 
     # Setting an if condition to check 
     if (player_choice in game_list):
-        print('You can play the game')
+        # Printing the choosen value on the screen
+        print("\nPlayer (" + player + ")" + " : " + "Computer (" + computer + ")")
+        # while loop to check if the game is a tie it should replay
+        while True:
+            if(player_choice == Computer)
         break    
     else:
         print("\nPlease Re-enter a valid choice from the above choices")
